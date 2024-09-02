a6b29909110716ae47b30abff1c5d35112de8b66
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-02 19:55:17 -0300

    Update Armário.lua

--- Sc/Armário.lua
+++ Sc/Armário.lua
@@ -1,80 +1,63 @@
- local TimeBetweenNotifications = 5 -- Substitua 5 pelo número de segundos que deseja esperar entre as notificações
+local esptable = { lockers = {} }
 
-        for i = 1, #Notifications do
-            local Notification = Notifications[i]
-            
-            StarterGui:SetCore("SendNotification", {
-                Title = "RSeekerHUB",
-                Text = Notification,
-                Duration = 5
-            })
-            
-            wait(TimeBetweenNotifications)
+local function ApplyLockerChams(inst)
+    local Cham = Instance.new("Highlight")
+    Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    Cham.FillColor = Color3.new(0.57, 0.39, 0.10) -- Cor do ESP (marrom claro)
+    Cham.FillTransparency = 0.5
+    Cham.OutlineColor = Color3.new(0.792156, 0.792156, 0.792156) -- Cor do contorno
+    Cham.Parent = game:GetService("CoreGui")
+    Cham.Adornee = inst
+    Cham.Enabled = true
+    Cham.RobloxLocked = true
+    return Cham
+end
+
+local function checkLocker(v)
+    if v:IsA("Model") then
+        task.wait(0.1)
+        if v.Name == "Wardrobe" or v.Name == "Rooms_Locker" or v.Name == "Rooms_Locker_Fridge" then
+            local h = ApplyLockerChams(v.PrimaryPart)
+            table.insert(esptable.lockers, h)
         end
+    end
+end
 
-        local esptable = {lockers={}}
-
-        local function ApplyLockerChams(inst)
-            local Cham = Instance.new("Highlight")
-            Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-            Cham.FillColor = Color3.new(0.57, 0.39, 0.10) -- Cor do ESP (marrom claro)
-            Cham.FillTransparency = 0.5
-            Cham.OutlineColor = Color3.new(0.792156, 0.792156, 0.792156) -- Cor do contorno
-            Cham.Parent = game:GetService("CoreGui")
-            Cham.Adornee = inst
-            Cham.Enabled = true
-            Cham.RobloxLocked = true
-            return Cham
-        end
-
-        local function checkLocker(v)
-            if v:IsA("Model") then
-                task.wait(0.1)
-                if v.Name == "Wardrobe" then
-                    local h = ApplyLockerChams(v.PrimaryPart)
-                    table.insert(esptable.lockers, h)
-                elseif (v.Name == "Rooms_Locker" or v.Name == "Rooms_Locker_Fridge") then
-                    local h = ApplyLockerChams(v.PrimaryPart)
-                    table.insert(esptable.lockers, h)
-                end
-            end
-        end
-
-        local function setupLockers(room)
-            local assets = room:WaitForChild("Assets")
-            
-            if assets then
-                local subaddcon
-                subaddcon = assets.DescendantAdded:Connect(function(v)
-                    checkLocker(v)
-                end)
-                
-                for i,v in pairs(assets:GetDescendants()) do
-                    checkLocker(v)
-                end
-                
-                task.spawn(function()
-                    repeat task.wait() until not flags.esplocker
-                    subaddcon:Disconnect()
-                end)
-            end
-        end
-
-        local addconnect
-        addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
-            setupLockers(room)
+local function setupLockers(room)
+    local assets = room:FindFirstChild("Assets")
+    
+    if assets then
+        local subaddcon
+        subaddcon = assets.DescendantAdded:Connect(function(v)
+            checkLocker(v)
         end)
-
-        for i,room in pairs(workspace.CurrentRooms:GetChildren()) do
-            setupLockers(room)
+        
+        for i, v in pairs(assets:GetDescendants()) do
+            checkLocker(v)
         end
+        
+        task.spawn(function()
+            repeat task.wait() until not flags.esplocker
+            subaddcon:Disconnect()
+        end)
+    end
+end
 
-        repeat task.wait() until not flags.esplocker
-        addconnect:Disconnect()
-        for i,v in pairs(esptable.lockers) do
-            v.Enabled = false
-            v.Adornee = nil
-            v:Destroy()
+local addconnect
+addconnect = workspace.CurrentRooms.ChildAdded:Connect(function(room)
+    setupLockers(room)
+end)
+
+for i, room in pairs(workspace.CurrentRooms:GetChildren()) do
+    setupLockers(room)
+end
+
+repeat task.wait() until not flags.esplocker
+addconnect:Disconnect()
+for i, v in pairs(esptable.lockers) do
+    v.Enabled = false
+    v.Adornee = nil
+    v:Destroy()
 end
 
 local sound = Instance.new("Sound")
@@ -85,7 +68,8 @@ sound:Play()
 sound.Ended:Connect(function()
     sound:Destroy()
 end)
-game:GetService("StarterGui"):SetCore("SendNotification", {
+
+StarterGui:SetCore("SendNotification", {
     Title = "🔔 Notificação",
     Text = "🗄️ Esp de armários ativo!",
     Icon = "rbxassetid://13264701341",
