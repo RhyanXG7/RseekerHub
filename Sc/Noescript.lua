d7040ca69541e4be60ad162d2ba29dacb5d0693e
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-05 17:59:36 -0300

    Update Noescript.lua

--- Sc/Noescript.lua
+++ Sc/Noescript.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGet("https://pastebin.com/raw/qH9PEhfQ"))()
 
 -- ERROR
 local sound = Instance.new("Sound")
-sound.SoundId = "rbxassetid://3458224686"
+sound.SoundId = "rbxassetid://5914602124"
 sound.Volume = 1
 sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 sound:Play()
