cb5ad3b6a5942115a28a129366fc16dd66580c33
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 05:22:30 -0300

    Create RushSum.lua

---
+++ Sc/RushSum.lua
@@ -0,0 +1,32 @@
+local function ExecuteCustomScript(inst)
+  
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "🔔 Notificação",
+    Text = "⚠️ Rush Nasceu, Esconda-se!",
+    Icon = "rbxassetid://13264701341",
+    Duration = 5
+})
+end
+
+Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
+    if inst.Name == "RushMoving" then
+        ExecuteCustomScript(inst)
+    end
+end)
+
+while true do
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "RushMoving" then
+            ExecuteCustomScript(v)
+        end
+    end
+    wait(1) 
+end
