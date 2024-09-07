94ea6491b02b82d7bdaeac68632aeefdc1cde66d
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 03:27:37 -0300

    Update RushEsp.lua

--- Sc/RushEsp.lua
+++ Sc/RushEsp.lua
@@ -2,32 +2,30 @@ local RushChams = {}
 local SelectedObject = nil
 
 local function ApplyRushChams(inst)
-    if not inst:IsDescendantOf(game.Workspace) then return nil end 
+    if not inst:IsDescendantOf(game.Workspace) then return nil end
     local Cham = Instance.new("Highlight")
     Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-    Cham.FillColor = Color3.new(0, 0, 0.5) 
+    Cham.FillColor = Color3.new(0, 0, 0.5)
     Cham.FillTransparency = 0.5
-    Cham.OutlineColor = Color3.new(0, 0, 0.5) 
+    Cham.OutlineColor = Color3.new(0, 0, 0.5)
     Cham.Adornee = inst
     Cham.Enabled = true
-    Cham.Parent = game:GetService("CoreGui")
-    Cham.RobloxLocked = true
+    Cham.Parent = inst
 
-    -- Adicionando a legenda "[Rush]" com ESP em azul escuro
     local BillboardGui = Instance.new("BillboardGui")
     BillboardGui.Adornee = inst
     BillboardGui.Size = UDim2.new(0, 100, 0, 50)
     BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-    BillboardGui.AlwaysOnTop = true 
-    BillboardGui.Parent = Cham
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.Parent = inst
 
     local Label = Instance.new("TextLabel")
     Label.Text = "[Rush]"
-    Label.TextColor3 = Color3.new(0, 0, 0.5) 
+    Label.TextColor3 = Color3.new(0, 0, 0.5)
     Label.BackgroundTransparency = 1
     Label.Size = UDim2.new(1, 0, 1, 0)
-    Label.TextScaled = false 
-    Label.TextSize = 14 
+    Label.TextScaled = false
+    Label.TextSize = 14
     Label.Parent = BillboardGui
 
     return Cham
@@ -66,6 +64,16 @@ for _, v in ipairs(Workspace:GetDescendants()) do
         OnObjectSelected(v)
     end
 end
+
+while true do
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "RushMoving" and not v:FindFirstChildOfClass("Highlight") then
+            OnObjectSelected(v)
+        end
+    end
+    wait(1)
+end
+
 
 -- Not
 
