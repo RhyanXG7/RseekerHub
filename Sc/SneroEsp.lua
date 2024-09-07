2b34986f5cfb5d6260efe21f5c24e04010a841b6
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 13:45:06 -0300

    Create SneroEsp.lua

---
+++ Sc/SneroEsp.lua
@@ -0,0 +1,86 @@
+local SnareChams = {}
+local SelectedObject = nil
+
+local function ApplySnareChams(inst)
+    if not inst:IsDescendantOf(game.Workspace) then return nil end
+    local Cham = Instance.new("Highlight")
+    Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+    Cham.FillColor = Color3.new(1, 0, 0)  -- Cor vermelha
+    Cham.FillTransparency = 0.5
+    Cham.OutlineColor = Color3.new(1, 0, 0)  -- Cor vermelha
+    Cham.Adornee = inst
+    Cham.Enabled = true
+    Cham.Parent = game:GetService("CoreGui")
+    Cham.RobloxLocked = true
+
+    local BillboardGui = Instance.new("BillboardGui")
+    BillboardGui.Adornee = inst
+    BillboardGui.Size = UDim2.new(0, 100, 0, 50)
+    BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.Parent = inst
+
+    local Label = Instance.new("TextLabel")
+    Label.Text = "[Snare]"
+    Label.TextColor3 = Color3.new(1, 0, 0)  -- Cor vermelha
+    Label.BackgroundTransparency = 1
+    Label.Size = UDim2.new(1, 0, 1, 0)
+    Label.TextScaled = false
+    Label.TextSize = 14
+    Label.Parent = BillboardGui
+
+    return Cham
+end
+
+local function OnObjectDeselected()
+    if SelectedObject then
+        for i = #SnareChams, 1, -1 do
+            local cham = SnareChams[i]
+            if cham.Adornee == SelectedObject then
+                cham:Destroy()
+                table.remove(SnareChams, i)
+            end
+        end
+        SelectedObject = nil
+    end
+end
+
+local function OnObjectSelected(inst)
+    OnObjectDeselected()
+    SelectedObject = inst
+    local cham = ApplySnareChams(inst)
+    if cham then
+        table.insert(SnareChams, cham)
+    end
+end
+
+Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
+    if inst.Name == "Snare" then
+        OnObjectSelected(inst)
+    end
+end)
+
+while true do
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "Snare" and not v:FindFirstChildOfClass("Highlight") then
+            OnObjectSelected(v)
+        end
+    end
+    wait(1)
+end
+
+-- Not
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "🔔 Notificação",
+    Text = "🌵 Esp Snare ativo!",
+    Icon = "rbxassetid://13264701341",
+    Duration = 5
+})
