7a00cb486a47b4a6686d29c4a1240361e5aeb226
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 05:24:46 -0300

    Update RushSum.lua

--- Sc/RushSum.lua
+++ Sc/RushSum.lua
@@ -30,3 +30,18 @@ while true do
     end
     wait(1) 
 end
+
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "🔔 Notificação",
+    Text = "⚠️ Notificação para rush ativa!",
+    Icon = "rbxassetid://13264701341",
+    Duration = 5
+})
