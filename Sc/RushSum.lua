b0b67d2acae9fa6efe6491f8f6b36bbe22f161e5
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 19:05:55 -0300

    Update RushSum.lua

--- Sc/RushSum.lua
+++ Sc/RushSum.lua
@@ -1,3 +1,52 @@
+local entities = {
+    "RushMoving"
+}
+local player = game.Players.LocalPlayer
+local hasNotified = false
+
+local function playNotif(soundId)
+    local Notification = Instance.new("Sound")
+    Notification.Parent = game.SoundService
+    Notification.SoundId = soundId or "rbxassetid://4590656842"
+    Notification.Volume = 1
+    Notification.PlayOnRemove = true
+    Notification:Play()
+    task.delay(2, function() 
+        Notification:Destroy()
+    end)
+end
+
+local function sendNotification(title, text, icon)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = title,
+        Text = text,
+        Icon = icon
+    })
+end
+
+workspace.ChildAdded:Connect(function(test)
+    if not hasNotified and table.find(entities, test.Name) then
+        local success, position = pcall(function() return test:GetPivot().Position end)
+        if success and position then
+            local connection
+            connection = game:GetService("RunService").Heartbeat:Connect(function()
+                if test:IsDescendantOf(workspace) then
+                    local distance = player:DistanceFromCharacter(position)
+                    if distance < 1000 then
+                        sendNotification("Rush surgiu", "Entre num armário!", "rbxassetid://10716032293")
+                        playNotif()
+                        hasNotified = true
+                        connection:Disconnect()
+                    end
+                else
+                    connection:Disconnect() 
+                end
+            end)
+        else
+            warn("O objeto não tem um Pivot válido.")
+        end
+    end
+end)
 
 -- Notificação 
 
