1739c510259411668f02c5979e100105284e67ee
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 03:42:28 -0300

    Update LivrosEsp.lua

--- Sc/LivrosEsp.lua
+++ Sc/LivrosEsp.lua
@@ -6,43 +6,54 @@ local function ApplyBookChams(inst)
     Cham.FillColor = Color3.new(1, 0, 0)
     Cham.FillTransparency = 0.5
     Cham.OutlineColor = Color3.new(0.792156, 0.792156, 0.792156)
-    Cham.Parent = game:GetService("CoreGui")
+    Cham.Parent = inst 
     Cham.Adornee = inst
     Cham.Enabled = true
-    Cham.RobloxLocked = true
 
     local BillboardGui = Instance.new("BillboardGui")
     BillboardGui.Adornee = inst
     BillboardGui.Size = UDim2.new(0, 100, 0, 50)
     BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
-    BillboardGui.AlwaysOnTop = true  -- Objeto visível através das paredes
-    BillboardGui.Parent = Cham
+    BillboardGui.AlwaysOnTop = true
+    BillboardGui.Parent = inst
 
     local Label = Instance.new("TextLabel")
     Label.Text = "[Livro]"
-    Label.TextColor3 = Color3.new(math.random(), math.random(), math.random())  -- Cor aleatória
+    Label.TextColor3 = Color3.new(math.random(), math.random(), math.random())
     Label.BackgroundTransparency = 1
     Label.Size = UDim2.new(1, 0, 1, 0)
-    Label.TextScaled = false  -- Desativar escalonamento do texto
-    Label.TextSize = 12  -- Tamanho da Fonte
+    Label.TextScaled = false
+    Label.TextSize = 12
     Label.Parent = BillboardGui
 
     return Cham
 end
 
 task.spawn(function()
-    task.wait(1)
-    for _, v in pairs(Workspace.CurrentRooms["50"].Assets:GetDescendants()) do
-        if v.Name == "LiveHintBook" then
-            ApplyBookChams(v)
+    while true do
+        task.wait(1)
+        local room = Workspace.CurrentRooms:FindFirstChild("50")
+        if room and room.Assets then
+            for _, v in pairs(room.Assets:GetDescendants()) do
+                if v.Name == "LiveHintBook" and not v:FindFirstChildOfClass("Highlight") then
+                    ApplyBookChams(v)
+                end
+            end
         end
     end
 end)
 
 task.spawn(function()
-    local Entity = Workspace.CurrentRooms["50"].FigureSetup:WaitForChild("FigureRagdoll", 5)
-    Entity:WaitForChild("Torso", 2.5)
-    ApplyBookChams(Entity)
+    while true do
+        task.wait(1)
+        local room = Workspace.CurrentRooms:FindFirstChild("50")
+        if room and room.FigureSetup then
+            local Entity = room.FigureSetup:WaitForChild("FigureRagdoll", 5)
+            if Entity and Entity:FindFirstChild("Torso") and not Entity:FindFirstChildOfClass("Highlight") then
+                ApplyBookChams(Entity)
+            end
+        end
+    end
 end)
 
 -- not
