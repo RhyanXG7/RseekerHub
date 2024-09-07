c436500b1907ae6c27bb0eb6a9de3d5bbf83e309
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-07 14:46:57 -0300

    Update RushSum.lua

--- Sc/RushSum.lua
+++ Sc/RushSum.lua
@@ -1,54 +1,54 @@
-local sound = Instance.new("Sound")
-sound.SoundId = "rbxassetid://7116606826"
+
+local processedObjects = {}
+
+local function ExecuteCustomScript(inst)
+    if not processedObjects[inst] then
+        processedObjects[inst] = true 
+        
+        local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
 sound.Volume = 1
 sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 sound:Play()
 sound.Ended:Connect(function()
     sound:Destroy()
 end)
-
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "🔔 Notificação",
-    Text = "⚠️ Notificação rush ativa!",
+    Text = "⚠️ Rush Nasceu, esconda-se!",
     Icon = "rbxassetid://13264701341",
     Duration = 5
 })
-
-wait(5)
-
-local notifiedEntities = {}
-
-local function ExecuteCustomScript(inst)
-    if not notifiedEntities[inst] then
-        notifiedEntities[inst] = true
-
-        local sound = Instance.new("Sound")
-        sound.SoundId = "rbxassetid://7116606826"
-        sound.Volume = 1
-        sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-        sound:Play()
-        sound.Ended:Connect(function()
-            sound:Destroy()
-        end)
-
-        game:GetService("StarterGui"):SetCore("SendNotification", {
-            Title = "🔔 Notificação",
-            Text = "⚠️ Rush Nasceu, Esconda-se!",
-            Icon = "rbxassetid://12443291836",
-            Duration = 5
-        })
+        print("Rush! : " .. inst.Name)
     end
 end
 
 Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
-    if inst.Name == "RushMoving" then
+    if inst.Name == "RushMoving" then 
         ExecuteCustomScript(inst)
     end
 end)
 
-for _, v in ipairs(Workspace:GetDescendants()) do
-    if v.Name == "RushMoving" and not notifiedEntities[v] then
-        ExecuteCustomScript(v)
+while true do
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "RushMoving" and not processedObjects[v] then
+            ExecuteCustomScript(v)
+        end
     end
+    wait(1) 
 end
 
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://3458224686"
+sound.Volume = 1
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
+sound.Ended:Connect(function()
+    sound:Destroy()
+end)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "🔔 Notificação",
+    Text = "👹 Avisos Rush Ativo!",
+    Icon = "rbxassetid://13264701341",
+    Duration = 5
+})
