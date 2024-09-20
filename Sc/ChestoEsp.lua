3c448423dd6c3f6e74d52e8bd2d8ee534ad460fb
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-20 00:13:41 -0300

    Update ChestoEsp.lua

--- Sc/ChestoEsp.lua
+++ Sc/ChestoEsp.lua
@@ -1,24 +1,23 @@
-local ChestChams = {}
-local SelectedObject = nil
+local ESPEnabled = _G.ChestBoxESPEnabled or false
+_G.ChestBoxESPEnabled = ESPEnabled
+local ChestBoxChams = {}
+local folder = Instance.new("Folder")
+folder.Name = "[ ChestBox : RSeekerHub ]"
+folder.Parent = game:GetService("CoreGui")
 
-local function ApplyChestChams(inst)
-    if not inst:IsDescendantOf(game.Workspace) then return nil end
-
+local function ApplyChestBoxChams(inst)
     local Cham = Instance.new("Highlight")
-    Cham.Name = "BAU ESP : SeekerHub"
     Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
     Cham.FillColor = Color3.new(0, 1, 1)
     Cham.FillTransparency = 0.5
-    Cham.OutlineColor = Color3.new(0, 1, 1)
-    Cham.OutlineTransparency = 0
+    Cham.OutlineColor = Color3.new(1, 1, 1)
     Cham.Adornee = inst
     Cham.Enabled = true
-    Cham.Parent = inst
+    Cham.Parent = folder
 
     local BillboardGui = Instance.new("BillboardGui")
-    BillboardGui.Name = "BAU LGD : SeekerHub"
     BillboardGui.Adornee = inst
-    BillboardGui.Size = UDim2.new(0, 50, 0, 25)
+    BillboardGui.Size = UDim2.new(0, 100, 0, 50)
     BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
     BillboardGui.AlwaysOnTop = true
     BillboardGui.Parent = inst
@@ -26,16 +25,48 @@ local function ApplyChestChams(inst)
     local Label = Instance.new("TextLabel")
     Label.Text = "[Baú]"
     Label.TextColor3 = Color3.new(0, 1, 1)
-    Label.Font = Enum.Font.GothamBold 
     Label.BackgroundTransparency = 1
     Label.Size = UDim2.new(1, 0, 1, 0)
-    Label.TextScaled = false
-    Label.TextSize = 12 
-    Label.TextStrokeTransparency = 0
-    Label.TextStrokeColor3 = Color3.new(0, 0, 0)
+    Label.TextSize = 14
+    Label.Font = Enum.Font.GothamBold
     Label.Parent = BillboardGui
 
     return Cham
+end
+
+local function OnObjectDeselected()
+    for i = #ChestBoxChams, 1, -1 do
+        local cham = ChestBoxChams[i]
+        cham:Destroy()
+        table.remove(ChestBoxChams, i)
+    end
+end
+
+local function OnObjectSelected(inst)
+    if ESPEnabled then
+        OnObjectDeselected()
+        local cham = ApplyChestBoxChams(inst)
+        table.insert(ChestBoxChams, cham)
+    end
+end
+
+if ESPEnabled then
+    folder.Parent = game:GetService("CoreGui")
+    Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
+        if inst.Name == "ChestBox" then
+            OnObjectSelected(inst)
+        end
+    end)
+
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "ChestBox" then
+            OnObjectSelected(v)
+        end
+    end
+end
+
+_G.ChestBoxESPEnabled = not ESPEnabled
+
 -- Notificação
 
 local sound = Instance.new("Sound")
