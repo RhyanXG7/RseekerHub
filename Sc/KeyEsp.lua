670501df21d2df49017126680fd711ace8ed0603
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-19 23:59:05 -0300

    Update KeyEsp.lua

--- Sc/KeyEsp.lua
+++ Sc/KeyEsp.lua
@@ -1,70 +1,71 @@
-local KeyChams = {}
-local SelectedObject = nil
+local ESPEnabled = _G.KeyObtainESPEnabled or false
+_G.KeyObtainESPEnabled = ESPEnabled
+local KeyObtainChams = {}
+local folder = Instance.new("Folder")
+folder.Name = "[ KeyObtain : RSeekerHub ]"
+folder.Parent = game:GetService("CoreGui")
 
-local function ApplyKeyChams(inst)
-    if not inst:IsDescendantOf(game.Workspace) then return nil end
+local function ApplyKeyObtainChams(inst)
     local Cham = Instance.new("Highlight")
     Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-    Cham.FillColor = Color3.new(0.980392, 0.670588, 0)
+    Cham.FillColor = Color3.new(0, 0, 1)
     Cham.FillTransparency = 0.5
-    Cham.OutlineColor = Color3.new(0.792156, 0.792156, 0.792156)
+    Cham.OutlineColor = Color3.new(1, 1, 1)
     Cham.Adornee = inst
     Cham.Enabled = true
-    Cham.Parent = game:GetService("CoreGui")
-    Cham.RobloxLocked = true
+    Cham.Parent = folder
 
     local BillboardGui = Instance.new("BillboardGui")
     BillboardGui.Adornee = inst
     BillboardGui.Size = UDim2.new(0, 100, 0, 50)
     BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
     BillboardGui.AlwaysOnTop = true
-    BillboardGui.Parent = inst  -- Alterado para inst
+    BillboardGui.Parent = inst
 
     local Label = Instance.new("TextLabel")
     Label.Text = "[Chave]"
     Label.TextColor3 = Color3.new(0, 0, 1)
     Label.BackgroundTransparency = 1
     Label.Size = UDim2.new(1, 0, 1, 0)
-    Label.TextScaled = false
     Label.TextSize = 14
+    Label.Font = Enum.Font.GothamBold
     Label.Parent = BillboardGui
 
     return Cham
 end
 
 local function OnObjectDeselected()
-    if SelectedObject then
-        for i = #KeyChams, 1, -1 do
-            local cham = KeyChams[i]
-            if cham.Adornee == SelectedObject then
-                cham:Destroy()
-                table.remove(KeyChams, i)
-            end
-        end
-        SelectedObject = nil
+    for i = #KeyObtainChams, 1, -1 do
+        local cham = KeyObtainChams[i]
+        cham:Destroy()
+        table.remove(KeyObtainChams, i)
     end
 end
 
 local function OnObjectSelected(inst)
-    OnObjectDeselected()
-    SelectedObject = inst
-    local cham = ApplyKeyChams(inst)
-    if cham then
-        table.insert(KeyChams, cham)
+    if ESPEnabled then
+        OnObjectDeselected()
+        local cham = ApplyKeyObtainChams(inst)
+        table.insert(KeyObtainChams, cham)
     end
 end
 
-Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
-    if inst.Name == "KeyObtain" then
-        OnObjectSelected(inst)
-    end
-end)
+if ESPEnabled then
+    folder.Parent = game:GetService("CoreGui")
+    Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
+        if inst.Name == "KeyObtain" then
+            OnObjectSelected(inst)
+        end
+    end)
 
-for _, v in ipairs(Workspace:GetDescendants()) do
-    if v.Name == "KeyObtain" then
-        OnObjectSelected(v)
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "KeyObtain" then
+            OnObjectSelected(v)
+        end
     end
 end
+
+_G.KeyObtainESPEnabled = not ESPEnabled
 
 -- Not
 
@@ -78,7 +79,7 @@ sound.Ended:Connect(function()
 end)
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "🔔 Notificação",
-    Text = "🔑 ESP KEY ATIVO!",
+    Text = "🔑 Chaves agora Visíveis.",
     Icon = "rbxassetid://13264701341",
     Duration = 5
 })
