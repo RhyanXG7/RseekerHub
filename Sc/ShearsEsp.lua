174fb6659bde20653d18206fbbc1e06a6bc723af
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-09-20 00:14:48 -0300

    Update ShearsEsp.lua

--- Sc/ShearsEsp.lua
+++ Sc/ShearsEsp.lua
@@ -1,22 +1,21 @@
+local ESPEnabled = _G.ShearsESPEnabled or false
+_G.ShearsESPEnabled = ESPEnabled
 local ShearsChams = {}
-local SelectedObject = nil
+local folder = Instance.new("Folder")
+folder.Name = "[ Shears : RSeekerHub ]"
+folder.Parent = game:GetService("CoreGui")
 
 local function ApplyShearsChams(inst)
-    if not inst:IsDescendantOf(game.Workspace) then return nil end
-
     local Cham = Instance.new("Highlight")
-    Cham.Name = "SHEARS ESP : SeekerHub"
     Cham.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
     Cham.FillColor = Color3.new(1, 1, 1)
     Cham.FillTransparency = 0.5
-    Cham.OutlineColor = Color3.new(0.8, 0.8, 0.8) 
-    Cham.OutlineTransparency = 0
+    Cham.OutlineColor = Color3.new(0, 0, 0)
     Cham.Adornee = inst
     Cham.Enabled = true
-    Cham.Parent = inst
+    Cham.Parent = folder
 
     local BillboardGui = Instance.new("BillboardGui")
-    BillboardGui.Name = "SHEARS LGD : SeekerHub"
     BillboardGui.Adornee = inst
     BillboardGui.Size = UDim2.new(0, 100, 0, 50)
     BillboardGui.StudsOffset = Vector3.new(0, 2, 0)
@@ -26,50 +25,50 @@ local function ApplyShearsChams(inst)
     local Label = Instance.new("TextLabel")
     Label.Text = "[Tesoura]"
     Label.TextColor3 = Color3.new(1, 1, 1)
-    Label.Font = Enum.Font.Fantasy
     Label.BackgroundTransparency = 1
     Label.Size = UDim2.new(1, 0, 1, 0)
-    Label.TextScaled = true
     Label.TextSize = 14
+    Label.Font = Enum.Font.GothamBold
     Label.Parent = BillboardGui
 
     return Cham
 end
 
 local function OnObjectDeselected()
-    if SelectedObject then
-        for i = #ShearsChams, 1, -1 do
-            local cham = ShearsChams[i]
-            if cham.Adornee == SelectedObject then
-                cham:Destroy()
-                table.remove(ShearsChams, i)
-            end
-        end
-        SelectedObject = nil
+    for i = #ShearsChams, 1, -1 do
+        local cham = ShearsChams[i]
+        cham:Destroy()
+        table.remove(ShearsChams, i)
     end
 end
 
 local function OnObjectSelected(inst)
-    OnObjectDeselected()
-    SelectedObject = inst
-    local cham = ApplyShearsChams(inst)
-    if cham then
+    if ESPEnabled then
+        OnObjectDeselected()
+        local cham = ApplyShearsChams(inst)
         table.insert(ShearsChams, cham)
     end
 end
 
-Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
-    if inst.Name == "Shears" then
-        OnObjectSelected(inst)
-    end
-end)
+if ESPEnabled then
+    folder.Parent = game:GetService("CoreGui")
+    Workspace.CurrentRooms.DescendantAdded:Connect(function(inst)
+        if inst.Name == "Shears" then
+            OnObjectSelected(inst)
+        end
+    end)
 
-for _, v in ipairs(Workspace:GetDescendants()) do
-    if v.Name == "Shears" then
-        OnObjectSelected(v)
+    for _, v in ipairs(Workspace:GetDescendants()) do
+        if v.Name == "Shears" then
+            OnObjectSelected(v)
+        end
     end
 end
--- not
+
+_G.ShearsESPEnabled = not ESPEnabled
+
+-- notificação
+
 local sound = Instance.new("Sound")
 sound.SoundId = "rbxassetid://3458224686"
 sound.Volume = 1
@@ -80,7 +79,7 @@ sound.Ended:Connect(function()
 end)
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "🔔 Notificação",
-    Text = "✂️ Esp tesoura ativo!",
+    Text = "✂️ Esp da tesoura agora ativo/desativado.",
     Icon = "rbxassetid://13264701341",
     Duration = 5
 })
